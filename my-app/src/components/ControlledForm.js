99eae4d831dc4f13c6942e83cacd2b32df4d38c2
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-11 22:08:31 +0000

    The form input is now controlled by react not dom but on submit its still controlled by DOM and refreshes the page

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -5,17 +5,29 @@ export class ControlledForm extends Component {
       super(props)
     
       this.state = {
-
+         name : ''
       }
     }
 
+    handleNameChange = (event) => {
+        // fetch the values from input field
+        this.setState({
+            name : event.target.value
+        })
+    }
   render() {
     return (
       <div>
         <form>
             {/* Uncntrolled form managed by DOM as it refreshes on submit button click */}
             <label htmlFor='id-name'>Your Name:</label>
-            <input id='id-name' name='name' />
+            <input 
+                value={this.state.name}
+                onChange={this.handleNameChange}
+                id='id-name'
+                name='name'
+                type='text'
+            />
             <input type='submit' value='Submit' />
         </form>
       </div>
